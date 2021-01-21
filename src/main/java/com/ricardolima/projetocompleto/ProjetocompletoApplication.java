c4a3ff0d77e14a0537af4b8c7c0a56fb6c25a4cf
ricardowirz <ricardowirz@msn.com>
2021-01-21 01:18:22 -0300

    Criando operação de instanciação

--- src/main/java/com/ricardolima/projetocompleto/ProjetocompletoApplication.java
+++ src/main/java/com/ricardolima/projetocompleto/ProjetocompletoApplication.java
@@ -1,13 +1,32 @@
 package com.ricardolima.projetocompleto;
 
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
-public class ProjetocompletoApplication {
+import com.ricardolima.projetocompleto.domain.Categoria;
+import com.ricardolima.projetocompleto.repositories.CategoriaRepository;
 
+@SpringBootApplication
+public class ProjetocompletoApplication implements CommandLineRunner{
+
+	@Autowired
+	private CategoriaRepository categoriaRepository;
+	
 	public static void main(String[] args) {
 		SpringApplication.run(ProjetocompletoApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		
+		Categoria cat1 = new Categoria(null, "Informática");
+		Categoria cat2 = new Categoria(null, "Escritório");
+		
+		categoriaRepository.saveAll(Arrays.asList(cat1, cat2));
 	}
 
 }
